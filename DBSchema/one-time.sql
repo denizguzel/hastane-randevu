864b9d82ee59d085d67771217e8499dc623eecf0
Okan <okan.uzun@outlook.com>
2017-03-09 09:27:40 +0300

    Some schema objects were imported into the database

---
+++ DBSchema/one-time.sql
@@ -0,0 +1,8 @@
+﻿-- Şema ve User kurulumu için.. SYSTEM kullanıcısıyla login olunup, bir kez çalıştırılacak..
+
+CREATE USER hospital IDENTIFIED BY hospital123;
+
+grant connect to hospital identified by hospital123;
+
+grant all privileges to hospital identified by hospital123;
+
