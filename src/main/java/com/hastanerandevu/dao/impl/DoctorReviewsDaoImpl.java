dbf1c0d56828c935511cbacdf25e2e82063eb6a5
Okan <okan.uzun@outlook.com>
2017-05-10 09:41:26 +0300

    services & daos added for doctor reviews

---
+++ src/main/java/com/hastanerandevu/dao/impl/DoctorReviewsDaoImpl.java
@@ -0,0 +1,32 @@
+package com.hastanerandevu.dao.impl;
+
+import com.hastanerandevu.model.DoctorModel;
+import com.hastanerandevu.model.PatientModel;
+import com.hastanerandevu.model.ReviewsAboutDoctorsModel;
+
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by ouzun on 5/10/2017.
+ */
+public class DoctorReviewsDaoImpl extends BaseDaoImpl<ReviewsAboutDoctorsModel> {
+  public List<ReviewsAboutDoctorsModel> getReviewsAboutDoctor(DoctorModel doctorModel){
+    Query query = getEntitymanager().createQuery("SELECT e FROM ReviewsAboutDoctorsModel e " +
+      "WHERE e.doctor = :DOCTOR_MODEL AND e.isAppropriate = :IS_APPROPRIATE ORDER BY e.creationTime DESC");
+
+    query.setParameter("DOCTOR_MODEL",doctorModel);
+    query.setParameter("IS_APPROPRIATE",'1');
+
+    return query.getResultList();
+  }
+
+  public List<ReviewsAboutDoctorsModel> getReviewsOfPatient(PatientModel patientModel){
+    Query query = getEntitymanager().createQuery("SELECT e FROM ReviewsAboutDoctorsModel e " +
+      "WHERE e.patient = :PATIENT_MODEL ORDER BY e.creationTime DESC");
+
+    query.setParameter("PATIENT_MODEL",patientModel);
+
+    return query.getResultList();
+  }
+}
---
+++ src/main/java/com/hastanerandevu/service/impl/DoctorReviewsServiceImpl.java
@@ -0,0 +1,49 @@
+package com.hastanerandevu.service.impl;
+
+import com.hastanerandevu.dao.impl.DoctorReviewsDaoImpl;
+import com.hastanerandevu.model.DoctorModel;
+import com.hastanerandevu.model.PatientModel;
+import com.hastanerandevu.model.ReviewsAboutDoctorsModel;
+import com.hastanerandevu.service.BaseService;
+
+import java.util.List;
+
+/**
+ * Created by ouzun on 5/10/2017.
+ */
+public class DoctorReviewsServiceImpl implements BaseService<ReviewsAboutDoctorsModel> {
+
+  DoctorReviewsDaoImpl doctorReviewsDao = new DoctorReviewsDaoImpl();
+
+  @Override
+  public void create(ReviewsAboutDoctorsModel model) {
+    doctorReviewsDao.create(model);
+  }
+
+  @Override
+  public void update(ReviewsAboutDoctorsModel model) {
+    doctorReviewsDao.update(model);
+  }
+
+  @Override
+  public void delete(ReviewsAboutDoctorsModel model) {
+    doctorReviewsDao.delete(model);
+  }
+
+  @Override
+  public ReviewsAboutDoctorsModel find(long id) {
+    return doctorReviewsDao.find(id);
+  }
+
+  @Override
+  public List<ReviewsAboutDoctorsModel> findAll() {
+    return doctorReviewsDao.findAll();
+  }
+
+  public List<ReviewsAboutDoctorsModel> getReviewsAboutDoctor(DoctorModel doctorModel){
+    return doctorReviewsDao.getReviewsAboutDoctor(doctorModel);
+  }
+  public List<ReviewsAboutDoctorsModel> getReviewsOfPatient(PatientModel patientModel){
+    return doctorReviewsDao.getReviewsOfPatient(patientModel);
+  }
+}
