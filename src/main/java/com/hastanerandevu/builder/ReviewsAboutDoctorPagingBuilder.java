2f9e73b2add03a7abd41f32ed843e73598998e66
Okan <okan.uzun@outlook.com>
2017-05-10 10:08:16 +0300

    builder added for paging doctor reviews

---
+++ src/main/java/com/hastanerandevu/builder/ReviewsAboutDoctorPagingBuilder.java
@@ -0,0 +1,15 @@
+package com.hastanerandevu.builder;
+
+/**
+ * Created by ouzun on 5/10/2017.
+ */
+public class ReviewsAboutDoctorPagingBuilder {
+  public static int numberOfPagingForReviews(int reviewsSize){
+    if (reviewsSize % 10 == 0){
+      return reviewsSize / 10;
+    }
+    else{
+      return (reviewsSize / 10) + 1;
+    }
+  }
+}
